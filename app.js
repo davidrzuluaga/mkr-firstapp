b4e3d2b6c9beba656b485d3252c279e50570ab5e
DavidRZuluaga <git@davidrzuluaga.com>
2019-08-19 17:20:51 -0500

    last test using pug

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.get('/', (req, res) => {
   let strings = []
   for (let i = 1;i<=50;i++) {
-    strings.push(`<p>${i} ${i%2 ? 'Soy Impar!' : 'Soy Par!'}!</p>`) 
+    strings.push(`${i} ${i%2 ? 'Soy Impar!' : 'Soy Par!'}!`) 
   }
-  res.send(`${strings.map(e=>e)}`);
+  res.render("index", { strings });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
