5025164ce3dd6d2550b190074cd5a702d923921e
DavidRZuluaga <git@davidrzuluaga.com>
2019-08-19 16:39:48 -0500

    params 2

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  const nombre = req.query.nombre || 'desconocido'
-  res.send(`<h1>Hola ${nombre}!</h1>`);
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+app.get('/makers/:nombre', (req, res) => {
+  const nombre = req.params.nombre || 'desconocido'
+  res.send(`<h1>Hola ${capitalizeFirstLetter(nombre)}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
