b9340a373486d1ec84e10cc239fe709d50f79c9e
DavidRZuluaga <git@davidrzuluaga.com>
2019-08-19 16:35:28 -0500

    query fix

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = req.params.name || 'desconocido'
-  res.send(`<h1>Hola ${name}!</h1>`);
+  const nombre = req.query.nombre || 'desconocido'
+  res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
